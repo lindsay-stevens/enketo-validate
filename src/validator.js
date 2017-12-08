15798a70430de230d454315ad2daf86fcef1e651
Martijn van de Rijdt <martijn@enketo.org>
2017-12-08 12:13:09 -0700

    refactored a bit

--- src/validator.js
+++ src/validator.js
@@ -7,36 +7,36 @@ const {
 } = require( 'xmldom' );
 
 
-let validate = xformStr => {
-    let warnings = [ 'warning 1' ];
+let parseXml = xmlStr => {
     let errors = [];
     const xmlParseError = 'XML Parse Error: ';
     const options = {
-        errorHandler: {
-            warning: function( w ) {
-                errors.push( xmlParseError + _cleanXmlDomParserError( w ) );
-            },
-            error: function( e ) {
-                errors.push( xmlParseError + _cleanXmlDomParserError( e ) );
-            },
-            fatalError: function( e ) {
-                errors.push( xmlParseError + _cleanXmlDomParserError( e ) );
-            }
-        }
+        // Treat warnings, errors and fatalErrors the same. Collect them all.
+        errorHandler: e => errors.push( xmlParseError + _cleanXmlDomParserError( e ) )
     };
+
     const parser = new DOMParser( options );
-    const doc = parser.parseFromString( xformStr, 'text/xml' );
+    let doc = parser.parseFromString( xmlStr, 'text/xml' );
 
     if ( errors.length ) {
         throw errors;
     }
 
+    return doc;
+};
+
+let validate = xformStr => {
+    let warnings = [ 'warning 1' ];
+    let doc = parseXml( xformStr );
+
     return warnings;
 };
 
 let _cleanXmlDomParserError = errorStr => {
-    const lineRef = errorStr.search( '\n@#' ) || undefined;
-    return errorStr.substring( errorStr.indexOf( '\t' ) + 1, lineRef ).replace( /!!/g, '!' );
+    const lineRefIndex = errorStr.search( '\n@#' ) || undefined;
+    return errorStr
+        .substring( errorStr.indexOf( '\t' ) + 1, lineRefIndex )
+        .replace( /!!/g, '!' );
 };
 
 module.exports = {
