8cac921bd092918a546f91068ce182b4256002a3
Martijn van de Rijdt <martijn@enketo.org>
2017-12-08 17:18:56 -0700

    start iterating through binds

--- src/validator.js
+++ src/validator.js
@@ -14,15 +14,34 @@ let validate = xformStr => {
         throw [ e ];
     }
 
-    const doc = xform.doc;
+    // Find binds
+    xform.binds.forEach( function( bind, index ) {
+        const path = bind.getAttribute( 'nodeset' );
+
+        if ( !path ) {
+            warnings.push( `Found bind (index: ${index}) without nodeset attribute.` );
+            return;
+        }
+        const nodeName = path.substring( path.lastIndexOf( '/' ) + 1 );
+
+        // Check if bind is bound to an existing node in the model.
+        const context = xform.getPrimaryInstanceNode( path );
+
+        if ( !context ) {
+            warnings.push( `Found bind for "${nodeName}" that does not exist in the model.` );
+            return;
+        }
+
+        console.log( 'found context for ', nodeName );
+    } );
 
     try {
-        console.log( 'find first label', xform.evaluate( 'ends-with(//label)' ) );
+        console.log( 'find first label', xform.evaluate( 'ends-with(//label, "a")' ) );
     } catch ( e ) {
         errors.push( e );
         console.error( 'evaluation failed:', e );
     }
-    if ( errors ) {
+    if ( errors.length ) {
         throw errors;
     }
     return warnings;
--- src/xform.js
+++ src/xform.js
@@ -7,22 +7,53 @@ const XPathJS = require( 'enketo-xpathjs' );
 class XForm {
 
     constructor( xformStr ) {
-        this.xformStr = xformStr;
+        if ( !xformStr || !xformStr.trim() ) {
+            throw 'Error: empty form.';
+        }
+        this.xformStr = this._deactivateDefaultNamespace( xformStr );
         this.dom = this._getDom();
         this.doc = this.dom.window.document;
         this.nsResolver = this._getNsResolver();
         this._bindXPathEvaluator();
     }
 
+    get binds() {
+        return this._getNodes( '/__h:html/__h:head/model/bind' );
+    }
+
+    getPrimaryInstanceNode( path ) {
+        const efficientPath = path.replace( /^(\/(?!model\/)[^\/][^\/\s,"']*\/)/, '/__h:html/__h:head/model/instance[1]$1' );
+        return this._getNode( efficientPath );
+    }
+
     evaluate( expr, context = this.doc ) {
         try {
             // We're only trying the Enketo XPath evaluator, not 'native' since we'd be testing the 
-            // "jsdom" module's implementation
+            // "jsdom" module's implementation which is not really native.
             return this.doc.evaluate( expr, context, this.nsResolver, 2, null ).stringValue;
         } catch ( e ) {
             //console.error( 'caught XPath exception', e );
             throw this._cleanXPathException( e );
         }
+    }
+
+    _getNodes( expr ) {
+        let nodes = [];
+        let result = this.doc.evaluate( expr, this.doc, this.nsResolver, 7, null );
+
+        for ( let j = 0; j < result.snapshotLength; j++ ) {
+            nodes.push( result.snapshotItem( j ) );
+        }
+
+        return nodes;
+    }
+
+    _getNode( path ) {
+        return this.doc.evaluate( path, this.doc, this.nsResolver, 9, 0 ).singleNodeValue;
+    }
+
+    _deactivateDefaultNamespace( xmlStr ) {
+        return xmlStr.replace( /\s(xmlns\=("|')[^\s\>]+("|'))/g, ' data-$1' );
     }
 
     _getDom() {
@@ -41,12 +72,12 @@ class XForm {
             //'xf': 'http://www.w3.org/2002/xforms',
             'orx': 'http://openrosa.org/xforms',
             'jr': 'http://openrosa.org/javarosa',
-            'enk': 'http://enketo.org/xforms'
+            'enk': 'http://enketo.org/xforms',
+            '__h': 'http://www.w3.org/1999/xhtml'
         };
 
         return {
             lookupNamespaceURI: function( prefix ) {
-                console.log( 'looking up ', prefix );
                 return namespaces[ prefix ] || null;
             }
         };
@@ -63,9 +94,9 @@ class XForm {
 
     _bindXPathEvaluator() {
         global.window = this.dom.window;
-        // Will the document overwrite cause a problem when the app runs in multiple threads?
+        // Looks like this overwrite will not cause a problem when the app runs "clustered" (multiple threads)
         global.document = this.dom.window.document;
-
+        // TODO: would be nice if we could pass window and document to XPathJS
         XPathJS.bindDomLevel3XPath();
     }
 }
